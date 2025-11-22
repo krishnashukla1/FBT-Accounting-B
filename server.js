e67bee83ae75ff5bab5e62c5f247fc5208c9d7a9
Krishna Shukla <backend.9developer@gmail.com>
2025-11-23 01:08:33 +0530

    cors error

--- server.js
+++ server.js
@@ -18,12 +18,16 @@ const app = express();
 // =============================
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: [
+      "http://localhost:5173",
+      "https://fbtaccounting.netlify.app"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
+
 
 app.use(express.json());
 
