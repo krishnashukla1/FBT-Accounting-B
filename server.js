4d7904bd3aaac89644932f4aba575968fc9c8050
krishnashukla1 <120076833+krishnashukla1@users.noreply.github.com>
2025-11-23 06:27:04 +0530

    initial commit1

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://fbtaccounting.netlify.app"
+      "https://fbtaccounting-web.netlify.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
